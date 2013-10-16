24678dfcbd9e4a414e6d49747795c9e1ec3f9719
Richard Huang <sdsoxfan@gmail.com>
2013-10-15 22:38:25 -0700

    Initial Commit

---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Richard Huang on 10/15/13.
+//  Copyright (c) 2013 Huang Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
